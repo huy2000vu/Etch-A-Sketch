e224dba545bc488398f7c5e8166ea676917d2de8
huy2000vu <huy2000vu@gmail.com>
2024-07-21 19:27:37 -0700

    minor formatting changes, added flex to settings

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 let gridsize 
 document.getElementById("sizebutton").onclick = () => {
     gridsize = document.getElementById("sizeInput").value
-    // console.log(gridsize)
     reset()
+    createGrids(gridsize);
+}
+function createGrids(size){
     const container = document.getElementById("container");
-for (let i = 0; i < gridsize*gridsize ; i++) {
+    for (let i = 0; i < size*size ; i++) {
     const grid = document.createElement("div");
     grid.classList.add("grids");
     grid.style.flex = `0 0 calc(100%/${gridsize})`;
-    grid.style.height = '100%/${gridsize}';
+    grid.style.height = `100%/${gridsize}`;
     container.appendChild(grid);
 }
-
 const grids = document.querySelectorAll(".grids");
 grids.forEach((grid)=>{
     grid.addEventListener("mouseover", () => {
@@ -19,14 +20,7 @@ grids.forEach((grid)=>{
         // console.log("grid.id");
     })
 })
-}
-
-
-
-
-
-
-function reset(){
+}function reset(){
     const container = document.getElementById("container");
     let child = container.lastChild;
     while(child){
@@ -34,3 +28,8 @@ function reset(){
         child = container.lastChild;
     }
 }
+
+// function realTimeDimensionValue()
+// {
+
+// }
