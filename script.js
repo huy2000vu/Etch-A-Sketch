16cdeef0e8a7a666a9a705099a368cb4111ee846
huy2000vu <huy2000vu@gmail.com>
2024-07-22 16:24:03 -0700

    added user's paint, background choice. +random paint color option

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let gridsize 
+//random color enabled
+const RandomColorInput = document.getElementById('randomColorBoolInput');
 document.getElementById("sizebutton").onclick = () => {
     gridsize = document.getElementById("sizeInput").value
     reset()
@@ -11,13 +13,18 @@ function createGrids(size){
     grid.classList.add("grids");
     grid.style.flex = `0 0 calc(100%/${gridsize})`;
     grid.style.height = `100%/${gridsize}`;
+    grid.style.backgroundColor = getUserBackgroundColor();
     container.appendChild(grid);
 }
 const grids = document.querySelectorAll(".grids");
 grids.forEach((grid)=>{
     grid.addEventListener("mouseover", () => {
-        grid.style.backgroundColor = "red";
-        // console.log("grid.id");
+        if(RandomColorInput.value) {
+            grid.style.backgroundColor = getRandomColor();
+        }
+        else{
+            grid.style.backgroundColor = getUserPaintColor();
+        }
     })
 })
 }function reset(){
@@ -29,7 +36,20 @@ grids.forEach((grid)=>{
     }
 }
 
-// function realTimeDimensionValue()
-// {
+function getUserPaintColor(){
+    paintColor = document.getElementById("paintcolorInput")
+    return paintColor.value;
+}
 
-// }
+function getUserBackgroundColor(){
+    backgroundColor = document.getElementById("BackgroundColorInput")
+    return backgroundColor.value;
+}
+
+function getRandomColor(){
+    // return R G B with random values --> rgb(255,255,255)
+    Rval = Math.floor(Math.random()*256)
+    Gval = Math.floor(Math.random()*256)
+    Bval = Math.floor(Math.random()*256)
+    return `rgb(${Rval}, ${Gval}, ${Bval})`
+}
